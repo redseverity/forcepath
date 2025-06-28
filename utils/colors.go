ae6292c080efed81b4cf3e9fc832101304091f22
redseverity <red.severity723@passfwd.com>
2025-06-28 00:45:27 -0300

    feat: add colors

---
+++ utils/colors.go
@@ -0,0 +1,14 @@
+package utils
+
+const (
+	ResetColor = "\033[0m"
+
+	BlackColor   = "\033[30m"
+	RedColor     = "\033[31m"
+	GreenColor   = "\033[32m"
+	YellowColor  = "\033[33m"
+	BlueColor    = "\033[34m"
+	MagentaColor = "\033[35m"
+	CyanColro    = "\033[36m"
+	WhiteColor   = "\033[37m"
+)
